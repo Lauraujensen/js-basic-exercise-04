59ae1330fe91c240f018abb7552e8d30e64dd03a
Lauraujensen <lauraujensen@hafnet.dk>
2025-01-27 09:33:02 +0100

    Opgave 4 løst

---
+++ js/script.js
@@ -0,0 +1,7 @@
+`use strict`;
+
+const welcomeText = `I'm learning to code with JavaScript`;
+
+const reassignmentText = `I want to become a good JavaScript programmer`;
+
+console.log(`${welcomeText}`);
